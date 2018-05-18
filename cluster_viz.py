1ea9dbfb183efb2a769f51a8611a10f82e62d698
Mohammad Amin Khashkhashi Moghaddam <amin.moghaddamv@gmail.com>
2018-05-18 16:23:38 +0430

    Add background and support for multiple plots
    Each node can now have a image attribute with the link to the image for background
    Plots are identified by a random id, this is necessary for multiple plots in jupyter

--- cluster_viz.py
+++ cluster_viz.py
@@ -1,17 +1,23 @@
 import json
+import random
+import string
 from string import Template
+
+
+def __get_random_id():
+    return ''.join(random.choice(string.ascii_lowercase + string.digits) for _ in range(6))
 
 
 def visualize(root, path, size=960):
     tmpl = Template(open('template.html').read())
     output_result = open(path, 'w')
-    output_result.write(tmpl.safe_substitute(json_root=json.dumps(root), size=size))
+    output_result.write(tmpl.safe_substitute(json_root=json.dumps(root), size=size, svg_id=__get_random_id()))
     output_result.close()
 
 
 def visualize_inline(root, size=960):
     tmpl = Template(open('template.html').read())
-    return tmpl.safe_substitute(json_root=json.dumps(root), size=size)
+    return tmpl.safe_substitute(json_root=json.dumps(root), size=size, svg_id=__get_random_id())
 
 
 def visualize_notebook(*args, **kwargs):
